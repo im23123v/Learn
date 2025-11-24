ad093afaeee1a83ab4986f72ed6d80a522767a2c
Vishwanath Karne <90135551+im23123v@users.noreply.github.com>
2025-11-24 12:17:09 +0530

    Update main[1].dart

--- main[1].dart
+++ main[1].dart
@@ -207,4 +207,666 @@
 //   }
 // }
 
-  
+
+2nd pdf
+
+1a : 
+
+void main() {
+  var name = 'I love you';
+  print('Hello, $name');
+}
+
+
+1b 
+
+
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(home: WidgetsDemo()));
+
+class WidgetsDemo extends StatelessWidget {
+  @override
+  Widget build(_) => Scaffold(
+    appBar: AppBar(title: Text('Widgets')),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(padding: EdgeInsets.all(12), color: Colors.blue[50],
+          child: Text('Simple Text', style: TextStyle(fontSize: 18))),
+        SizedBox(height: 12),
+        Image.network('https://flutter.dev/assets/images/shared/brand/flutter/logo/flutter-lockup.png', width:120),
+      ],
+    ),
+  );
+}
+
+
+
+2 a      
+
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(home: Layouts()));
+
+class Layouts extends StatelessWidget {
+  @override
+  Widget build(_) => Scaffold(
+    appBar: AppBar(title: Text('Row/Column/Stack')),
+    body: Padding(
+      padding: EdgeInsets.all(12),
+      child: Column(children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          Text('R1'), Text('R2'), Text('R3')
+        ]),
+        SizedBox(height:20),
+        Column(children: [Text('C1'), Text('C2')]),
+        SizedBox(height:20),
+        SizedBox(height:120, width: double.infinity,
+          child: Stack(children: [
+            Container(color: Colors.yellow),
+            Positioned(top:10,left:10, child: Text('Top')),
+            Positioned(bottom:10,right:10, child: Text('Bottom')),
+          ]),
+        )
+      ]),
+    ),
+  );
+}
+
+
+
+2 b row,column,stack
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Row Layout'),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+            ),
+            Container(
+              color: Colors.green,
+              width: 100,
+              height: 100,
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+3a narrow wide
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Responsive UI Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ResponsiveHomePage(),
+    );
+  }
+}
+
+class ResponsiveHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Responsive UI Demo'),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return _buildNarrowLayout();
+          } else {
+            return _buildWideLayout();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildNarrowLayout() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlutterLogo(size: 100),
+          SizedBox(height: 20),
+          Text(
+            'Narrow Layout',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Button'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWideLayout() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlutterLogo(size: 100),
+          SizedBox(width: 20),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Wide Layout',
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Button'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+3b
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Responsive UI with Media Queries',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ResponsiveHomePage(),
+    );
+  }
+}
+
+class ResponsiveHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Responsive UI with Media Queries'),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return _buildMobileLayout();
+          } else if (constraints.maxWidth < 1200) {
+            return _buildTabletLayout();
+          } else {
+            return _buildDesktopLayout();
+          }
+        },
+      ),
+    );
+  }
+
+  // ---------------- MOBILE ----------------
+  Widget _buildMobileLayout() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlutterLogo(size: 100),
+          SizedBox(height: 20),
+          Text(
+            'Mobile Layout',
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Button'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // ---------------- TABLET ----------------
+  Widget _buildTabletLayout() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlutterLogo(size: 100),
+          SizedBox(width: 20),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Tablet Layout',
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Button'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // ---------------- DESKTOP ----------------
+  Widget _buildDesktopLayout() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlutterLogo(size: 150),
+          SizedBox(width: 40),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Desktop Layout',
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Button'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+4a  screen switch
+
+
+import 'package:flutter/material.dart';
+
+
+// void main() => runApp(NavigationApp());
+// class NavigationApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(home: FirstScreen());
+//   }
+// }
+// class FirstScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("First Screen")),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             Navigator.push(
+//                 context, MaterialPageRoute(builder: (context) => SecondScreen()));
+//           },
+//           child: Text("Go to Second Screen"),
+//         ),  ),  );
+//   }
+// }
+// class SecondScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Second Screen")),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () => Navigator.pop(context),
+//           child: Text("Back to First Screen"),
+//         ), ), );
+//   }
+// }
+
+
+
+
+4b 
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> _data = [];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDataFromApi();
+  }
+
+  Future<void> _fetchDataFromApi() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final response = await http.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _data = json.decode(response.body);
+        _isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('API Data Example'),
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _data.length,
+              itemBuilder: (context, index) {
+                return PostCard(
+                  title: _data[index]['title'],
+                  body: _data[index]['body'],
+                );
+              },
+            ),
+    );
+  }
+}
+
+class PostCard extends StatelessWidget {
+  final String title;
+  final String body;
+
+  const PostCard({
+    Key? key,
+    required this.title,
+    required this.body,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              body,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+5a stateless widget
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cards Example'),
+        ),
+        body: CardList(),
+      ),
+    );
+  }
+}
+
+class CardList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return CardItem(
+          title: 'Title $index',
+          subtitle: 'Subtitle $index',
+        );
+      },
+    );
+  }
+}
+
+class CardItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const CardItem({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(title.substring(0, 1)),
+        ),
+        title: Text(title),
+        subtitle: Text(subtitle),
+        onTap: () {
+          // Handle tap if needed
+        },
+      ),
+    );
+  }
+}
+
+5a statefull widget
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterApp(),
+    );
+  }
+}
+
+class CounterApp extends StatefulWidget {
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Counter:',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+
+5b  setState
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Example (setState)'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Counter Value:',
+              style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
